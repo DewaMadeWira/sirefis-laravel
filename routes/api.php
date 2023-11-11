b97623817d38f7491acbe9b1afdaae600fc7034b
Dewa made <108676698+DewaMadeWira@users.noreply.github.com>
2023-11-11 15:57:00 +0700

    add ranking in flask

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,13 @@
 <?php
 
+// use cors;
+
+
 use App\Models\Gpu;
-
-
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Route;
+use GuzzleHttp\Client;
 // use App\Http\Controllers\APIController;
 
 /*
@@ -27,3 +30,52 @@ Route::get('gpu', function(){
     return Gpu::all();
     // return "hello";
 });
+Route::get('post-rank', function(){
+    $data = Gpu::all();
+    $response = Http::post('http://127.0.0.1:5000/post-rank',["gpu_data"=>$data]);
+    return $response;
+    // return Gpu::all();
+    // return "hello";
+});
+Route::post('gpu', function(Request $request){
+    $jsonData = $request->json()->all();
+    return $jsonData;
+    // $name = $request->name;
+    // return $name;
+    // return Gpu::all();
+    // return "hello";
+});
+
+Route::get('rank-gpu', function(){
+    $response = Http::get('http://127.0.0.1:5000/');
+    // return "requested";
+    return $response;
+    // return "hello";
+});
+// Route::get('rank-gpu', function(){
+    
+//     // return Gpu::all();
+//     // return "hello";
+
+//     // $response = Http::get('http://127.0.0.1:5000/');
+//     $response = Http::withHeaders([
+//     'Origin' => 'http://127.0.0.1:5000/'  // Replace with your Flask API local URL
+// ])->get('http://127.0.0.1:5000/'); 
+//     return $response;
+// });
+
+// Route::middleware(['cors'])->group(function () {
+//     // Define your routes here
+//     Route::get('rank-gpu', function () {
+//         $client = new Client();
+
+//         $response = $client->request('GET', 'http://127.0.0.1:5000/');
+
+//         $body = $response->getBody();
+//         $jsonResponse = json_decode($body, true);
+//         return  $jsonResponse;
+//         // $response = Http::get('http://127.0.0.1:5000/');
+//         // return $response;
+//     });
+// });
+
