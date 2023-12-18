695f01a4ec3e08f26fb15253412e3052163879b1
Dewa made <108676698+DewaMadeWira@users.noreply.github.com>
2023-12-18 20:45:38 +0700

    add cors

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -21,7 +21,6 @@ class CorsMiddleware
             ->header('Access-Control-Allow-Origin', '*')
             ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
             ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-
         return $response;
     }
 }
